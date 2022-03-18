e7f5f694391ed0fde80e734d82d4ef634d51ebf2
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-03-18 19:39:25 +0600

    commont

--- js/countries.js
+++ js/countries.js
@@ -1,1 +1,20 @@
 
+const loadCountries = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+};
+const displayCountries = countries => {
+  console.log(countries)
+  const countriesHTML = countries.map(country => getCountryHTML(country));
+  console.log(countriesHTML[0]);
+}
+const getCountryHTML = country => {
+  return `
+    <div>
+      <h2>${country.name.common}</h2>
+      <h2>${country.name.official}</h2>
+    </div>
+  `;
+}
+loadCountries();
