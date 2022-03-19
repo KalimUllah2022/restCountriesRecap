18ea19b2cc69222284e82d8531a3d37362e0b1ca
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-03-19 14:46:14 +0600

    commit

--- js/countries.js
+++ js/countries.js
@@ -1,13 +1,11 @@
-/** @format */
-
 const loadCountries = () => {
   fetch("https://restcountries.com/v3.1/all")
-    .then((res) => res.json())
+    .then((response) => response.json())
     .then((data) => displayCountries(data));
-};
+}
 const displayCountries = countries => {
-  console.log(countries[0])
-  const countriesHTML = countries.map((country) => getCountryHTML(country));
+  // console.log(countries)
+  const countriesHTML = countries.map(country => getCountryHTML(country));
   // console.log(countriesHTML[0]);
   const container = document.getElementById("countries");
   container.innerHTML = countriesHTML.join(' ');
@@ -16,10 +14,8 @@ const getCountryHTML = country => {
   return `
     <div class="country">
       <h2>${country.name.common}</h2>
-      <h4>Capital: ${country.capital}</h4>
-      <h5>${country.population}</h5>
       <img src="${country.flags.png}">
     </div>
-  `;
+  `
 }
 loadCountries();
