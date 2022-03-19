d5a71a4ef28ca55a465012809eb4558a71be1413
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-03-19 17:46:22 +0600

    commit

--- js/countries.js
+++ js/countries.js
@@ -1,21 +1,43 @@
+/** @format */
+
 const loadCountries = () => {
   fetch("https://restcountries.com/v3.1/all")
     .then((response) => response.json())
     .then((data) => displayCountries(data));
-}
-const displayCountries = countries => {
-  // console.log(countries)
-  const countriesHTML = countries.map(country => getCountryHTML(country));
+};
+const displayCountries = (countries) => {
+  console.log(countries[0]);
+  const countriesHTML = countries.map((country) => getCountryHTML(country));
   // console.log(countriesHTML[0]);
   const container = document.getElementById("countries");
-  container.innerHTML = countriesHTML.join(' ');
-}
-const getCountryHTML = country => {
+  container.innerHTML = countriesHTML.join(" ");
+};
+const getCountryHTML = (name, flags) => {
   return `
     <div class="country">
-      <h2>${country.name.common}</h2>
-      <img src="${country.flags.png}">
+      <h2>${name.common}</h2>
+      <img src="${flags.png}">
     </div>
-  `
-}
+  `;
+};
+
+// Original
+// const getCountryHTML = (country) => {
+//   return `
+//     <div class="country">
+//       <h2>${country.name.common}</h2>
+//       <img src="${country.flags.png}">
+//     </div>
+//   `;
+// };
+// // Option1
+// const getCountryHTML = (country) => {
+//   const { name, flags } = country;
+//   return `
+//     <div class="country">
+//       <h2>${name.common}</h2>
+//       <img src="${flags.png}">
+//     </div>
+//   `;
+// };
 loadCountries();
